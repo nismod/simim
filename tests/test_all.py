93f1cf745e8b161cfcb0d0f80bf3469822ca5c3a
virgesmith <a.p.smith@leeds.ac.uk>
2018-12-04 21:05:55 +0000

    more tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -11,6 +11,8 @@ from pysal.contrib.spint.gravity import Gravity
 from pysal.contrib.spint.gravity import Attraction
 from pysal.contrib.spint.gravity import Doubly
 
+from simim.utils import r2, rmse
+
 # test methods only run if prefixed with "test"
 class Test(TestCase):
   """ Test harness """
@@ -18,30 +20,44 @@ class Test(TestCase):
   # GB dataset of population, households and inter-LAD distances
   dataset = pd.read_csv("tests/data/test2011.csv.gz")
 
+  def test_stats(self):
+    np.random.seed(0)
+    x = np.random.normal(0,1,10000)
+    y = np.random.normal(0,1,10000)
+
+    # R2
+    self.assertEqual(r2(x,x), 1.0)
+    self.assertEqual(r2(x,-x), 1.0) # rho=-1 
+    self.assertTrue(-0.0001 < r2(x,y) < 0.0001)
+    # 
+    rho = 0.5
+    res = r2(x, rho * x + np.sqrt(1 - rho**2) * y)
+    self.assertTrue(rho**2 - 0.015 < res < rho**2 + 0.015)
+
+    z = np.zeros(10000)
+    self.assertEqual(rmse(x,z), np.sqrt(np.mean(x*x)))
+    self.assertEqual(rmse(x,x), 0.0)
+    self.assertEqual(rmse(x,-x), np.sqrt(np.mean(4*x*x)))
+
+
   def test_dataset(self):
     self.assertTrue(len(Test.dataset) == 378*377)
 
   def test_gravity(self):
+    for model in ["pow", "exp"]:
+      gravity = Gravity(Test.dataset.MIGRATIONS.values, Test.dataset.PEOPLE.values, Test.dataset.HOUSEHOLDS.values, Test.dataset.DISTANCE.values, model)
+      k = gravity.params[0]
+      mu = gravity.params[1]
+      alpha = gravity.params[2]
+      beta = gravity.params[3]
 
-    model = "pow"
-    print("model: " + model)
-    gravity = Gravity(Test.dataset.MIGRATIONS.values, Test.dataset.PEOPLE.values, Test.dataset.HOUSEHOLDS.values, Test.dataset.DISTANCE.values, model)
-    #print(gravity.params)
-    # TODO this formula is wrong?
-    k = gravity.params[0]
-    mu = gravity.params[1]
-    alpha = gravity.params[2]
-    beta = gravity.params[3]
-    if model == "pow":
-      est_unc = (np.exp(k) * Test.dataset.PEOPLE ** mu * Test.dataset.HOUSEHOLDS ** alpha * Test.dataset.DISTANCE ** beta).values
-    else:
-      est_unc = (np.exp(k) * Test.dataset.PEOPLE ** mu * od_2011.HOUSEHOLDS ** alpha * np.exp(Test.dataset.DISTANCE * beta)).values
+      if model == "pow":
+        est_unc = (np.exp(k) * Test.dataset.PEOPLE ** mu * Test.dataset.HOUSEHOLDS ** alpha * Test.dataset.DISTANCE ** beta).values
+      else:
+        est_unc = (np.exp(k) * Test.dataset.PEOPLE ** mu * Test.dataset.HOUSEHOLDS ** alpha * np.exp(Test.dataset.DISTANCE * beta)).values
 
-    self.assertTrue(np.mean(est_unc - gravity.yhat) < 2e-16)
-    self.assertTrue(np.sqrt(np.mean((est_unc - gravity.yhat)**2)) < 1e-13)
-    # 1.4330978320718025e-16
-    # 7.559644320891063e-14
-
+      self.assertTrue(np.mean(est_unc - gravity.yhat) < 2e-16)
+      self.assertTrue(np.sqrt(np.mean((est_unc - gravity.yhat)**2)) < 1e-13)
 
 if __name__ == "__main__":
   unittest.main()
