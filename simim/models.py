5fcf34cb793de724495e34ff1b0e54286885e69d
virgesmith <a.p.smith@leeds.ac.uk>
2018-12-04 16:51:13 +0000

    start adding tests

---
+++ simim/models.py
@@ -0,0 +1,8 @@
+""" models.py """
+
+from pysal.contrib.spint.gravity import Gravity
+from pysal.contrib.spint.gravity import Attraction
+from pysal.contrib.spint.gravity import Doubly
+
+
+def gravity():
