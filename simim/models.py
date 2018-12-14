036e4976c16d568931960d0d0811af27d161534d
virgesmith <a.p.smith@leeds.ac.uk>
2018-12-14 09:20:20 +0000

    add tests for prod/attr (re)evaluation

--- simim/models.py
+++ simim/models.py
@@ -10,38 +10,30 @@ from pysal.contrib.spint.gravity import Doubly
 _valid_types = ["gravity", "production", "attraction", "doubly"]
 _valid_subtypes = ["pow", "exp"]
 
-def validate(model_type, model_subtype):
+def validate(model_type, model_subtype, dataset):
   if not model_type in _valid_types:
     raise ValueError("invalid model type % (must be one of %s)" % (model_subtype, str(_valid_types)))
   if not model_subtype in _valid_subtypes:
     raise ValueError("invalid model subtype % (must be one of %s)" % (model_subtype, str(_valid_subtypes)))
-
-  # TODO perturb
-  #  od_2011.loc[od_2011.D_GEOGRAPHY_CODE == "E07000178", "HOUSEHOLDS"] = od_2011.loc[od_2011.D_GEOGRAPHY_CODE == "E07000178", "HOUSEHOLDS"] + 300000 
-  #  pert_unc = ((np.exp(k) * od_2011.PEOPLE ** mu * od_2011.HOUSEHOLDS ** alpha * od_2011.DISTANCE ** beta).values + 0.5).astype(int)
-  # can only affect rows with the perturbed destination
-  # print(np.count_nonzero(pert_unc - est_unc)/len(est_unc)) # =1/378
-  # # re-fit model
-  # gravity_bumped = Gravity(od_2011.MIGRATIONS.values, od_2011.PEOPLE.values, od_2011.HOUSEHOLDS.values, od_2011.DISTANCE.values, model)
-  # print(gravity.params)
-  # print(gravity_bumped.params)
-  # k = gravity_bumped.params[0]
-  # mu = gravity_bumped.params[1]
-  # alpha = gravity_bumped.params[2]
-  # beta = gravity_bumped.params[3]
-  # bump_unc = ((np.exp(k) * od_2011.PEOPLE ** mu * od_2011.HOUSEHOLDS ** alpha * od_2011.DISTANCE ** beta).values + 0.5).astype(int)
-  # print("Bumped Unconstrained Poisson Fitted R2 = %f" % gravity_bumped.pseudoR2)
-  # print("Bumped Unconstrained Poisson Fitted RMSE = %f" % gravity_bumped.SRMSE)
+  if not "O_GEOGRAPHY_CODE" in dataset.columns.values:
+    raise ValueError("dataset must contain an O_GEOGRAPHY_CODE column for origin identifiers")
+  if not "D_GEOGRAPHY_CODE" in dataset.columns.values:
+    raise ValueError("dataset must contain an D_GEOGRAPHY_CODE column for destination identifiers")
 
 
 class Model:
   def __init__(self, model_type, model_subtype, dataset, y_col, xo_cols, xd_cols, cost_col):
     self.model_type = model_type
     self.model_subtype = model_subtype
-    validate(self.model_type, self.model_subtype)
+    validate(self.model_type, self.model_subtype, dataset)
 
-    # take a copy of the input dataset
     self.dataset = dataset.copy()
+    # TODO needs more thought...
+    # take a copy of the input dataset and ensure sorted by D then O
+    # so that the ordering of mu, alpha is determined
+    #self.dataset = dataset.sort_values(["D_GEOGRAPHY_CODE", "O_GEOGRAPHY_CODE"])
+    # now we have copy, invalidate the input!!!
+    #dataset = None
 
     self.y_col = y_col
     self.xo_cols = xo_cols
@@ -68,6 +60,7 @@ class Model:
                              self.dataset[self.xo_cols].values, 
                              self.dataset[self.cost_col].values, self.model_subtype)
     else: #model_type == "doubly":
+      raise NotImplemented("Doubly constrained model is too constrained")
       self.impl = Doubly(self.dataset[self.y_col].values, 
                          self.dataset[self.xo_cols].values, 
                          self.dataset[self.xd_cols].values, 
@@ -137,13 +130,10 @@ class Model:
     elif self.model_type == "production":
       #assert xo is None
       assert xd is not None
-      #mu = np.repeat(self.mu(), int(len(self.dataset)/len(self.mu())))
       mu = np.append(0, self.mu())
       mu = np.tile(mu, int(len(self.dataset)/len(mu)))
-      # NB ordering is not guaranteed
-      self.dataset["mu"] = np.exp(mu)
-      #mu = np.repeat(0, len(self.dataset))
       assert len(mu) == len(self.dataset)
+      # NB ordering is only guaranteed if dataset is sorted by origin then destination code
       if self.model_subtype == "pow":
         ybar = (np.exp(self.k()) * np.exp(mu) * xd ** self.alpha() * self.dataset[self.cost_col] ** self.beta())
       else:
@@ -154,15 +144,13 @@ class Model:
       #assert xd is None
       alpha = np.append(0,self.alpha())
       alpha = np.repeat(alpha, int(len(self.dataset)/len(alpha)))
-      # NB ordering is not guaranteed
-      self.dataset["alpha"] =np.exp(alpha)
-      #alpha = np.tile(self.alpha(), int(len(self.dataset)/len(self.alpha())))
       assert len(alpha) == len(self.dataset)
+      # NB ordering is only guaranteed if dataset is sorted by origin then destination code
       if self.model_subtype == "pow":
         ybar = (np.exp(self.k()) * xo ** self.mu() * np.exp(alpha) * self.dataset[self.cost_col] ** self.beta())
       else:
         ybar = (np.exp(self.k()) * xo ** self.mu() * np.exp(alpha) * np.exp(self.dataset[self.cost_col] * self.beta()))
       return ybar
     else:
-      raise NotImplementedError("TODO...")
+      raise NotImplementedError("%s evaluation not implemented" % self.model_type)
 
--- tests/test_all.py
+++ tests/test_all.py
@@ -19,7 +19,7 @@ class Test(TestCase):
   """ Test harness """
 
   # GB dataset of population, households, jobs and inter-LAD distances
-  dataset = pd.read_csv("tests/data/testdata.csv.gz")
+  dataset = pd.read_csv("tests/data/testdata.csv.gz").sort_values(["D_GEOGRAPHY_CODE", "O_GEOGRAPHY_CODE"])
 
   def test_stats(self):
     np.random.seed(0)
@@ -39,7 +39,6 @@ class Test(TestCase):
     self.assertEqual(rmse(x,z), np.sqrt(np.mean(x*x)))
     self.assertEqual(rmse(x,x), 0.0)
     self.assertEqual(rmse(x,-x), np.sqrt(np.mean(4*x*x)))
-
 
   def test_dataset(self):
     self.assertTrue(len(Test.dataset) == 378*378)
@@ -63,6 +62,24 @@ class Test(TestCase):
       gravity = models.Model("gravity", model_subtype, Test.dataset, "MIGRATIONS", ["PEOPLE", "JOBS"], "HOUSEHOLDS", "DISTANCE")
       print(gravity.impl.params)
       self.assertTrue(rmse(gravity([Test.dataset.PEOPLE.values, Test.dataset.JOBS.values], Test.dataset.HOUSEHOLDS.values), gravity.impl.yhat) < 1e-10)
+
+  def test_production(self):
+    # single factor attr
+    for model_subtype in ["pow", "exp"]:
+      production = models.Model("production", model_subtype, Test.dataset, "MIGRATIONS", "O_GEOGRAPHY_CODE", "HOUSEHOLDS", "DISTANCE")
+      self.assertTrue(rmse(production(xd=Test.dataset.HOUSEHOLDS.values), production.impl.yhat) < 1e-10)
+      Test.dataset["HH_CHANGED"] = Test.dataset.HOUSEHOLDS
+      Test.dataset.loc[Test.dataset.D_GEOGRAPHY_CODE == "E07000178", "HH_CHANGED"] = Test.dataset.loc[Test.dataset.D_GEOGRAPHY_CODE == "E07000178", "HOUSEHOLDS"] + 300000 
+      self.assertTrue(rmse(production(xd=Test.dataset.HH_CHANGED.values), production.impl.yhat) > 1.0)
+
+  def test_attraction(self):
+    # single factor prod
+    for model_subtype in ["pow", "exp"]:
+      attraction = models.Model("attraction", model_subtype, Test.dataset, "MIGRATIONS", "PEOPLE", "D_GEOGRAPHY_CODE", "DISTANCE")
+      self.assertTrue(rmse(attraction(xo=Test.dataset.PEOPLE.values), attraction.impl.yhat) < 1e-10)
+      Test.dataset["P_CHANGED"] = Test.dataset.PEOPLE
+      Test.dataset.loc[Test.dataset.D_GEOGRAPHY_CODE == "E07000178", "P_CHANGED"] = Test.dataset.loc[Test.dataset.D_GEOGRAPHY_CODE == "E07000178", "HOUSEHOLDS"] + 300000 
+      self.assertTrue(rmse(attraction(xo=Test.dataset.P_CHANGED.values), attraction.impl.yhat) > 1.0)
 
 if __name__ == "__main__":
   unittest.main()
