00d8b0b488116b9fd0e3ee0354917cb45c47c1b7
virgesmith <a.p.smith@leeds.ac.uk>
2019-05-14 13:20:10 +0100

    fix scenario generation script, add scenario 0

--- scripts/generate_econ_scenarios.py
+++ scripts/generate_econ_scenarios.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 
-scenario_names = ["scenario1", "scenario2"]
+scenario_names = ["scenario0", "scenario1", "scenario2"]
 
 for scenario_name in scenario_names:
   h_scenario = pd.read_csv("data/scenarios/%s.csv" % scenario_name)
@@ -11,7 +11,7 @@ for scenario_name in scenario_names:
   baseline = pd.read_csv("data/ce_gva_employment_baseline.csv")
   baseline = baseline[baseline.lad18cd.isin(arclads)].drop(["lad11nm", "lad11cd", "lad18nm"], axis=1).set_index(["year", "lad18cd"])
 
-  scenario = pd.read_csv("data/arc_gva_employment__scenario2.csv")
+  scenario = pd.read_csv("data/arc_gva_employment__%s.csv" % scenario_name)
   scenario = scenario[scenario.lad18cd.isin(arclads)].drop(["lad11nm", "lad11cd", "lad18nm"], axis=1).set_index(["year", "lad18cd"])
 
   scenario = scenario.join(baseline, lsuffix="_scen", rsuffix="_base")
