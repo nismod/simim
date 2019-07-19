1274c1a065d971385eccd86e8a4a231994e4534e
Tom Russell <tomalrussell@gmail.com>
2019-07-19 10:03:06 +0100

    Plot two zones timeseries, only actual and diff OD

--- scripts/run.py
+++ scripts/run.py
@@ -38,15 +38,18 @@ def main(params):
     # fig.suptitle("UK LAD SIMs using population as emitter, households as attractor")
     v = visuals.Visual(2,3)
 
-    v.scatter((0,0), model.dataset.MIGRATIONS, model.impl.yhat, "b.", markersize=3, 
-      title="%d %s migration model fit: R^2=%.2f" % (year, params["model_type"], model.impl.pseudoR2))
-    v.line((0,0), [0,max(model.dataset.MIGRATIONS)], [0,max(model.dataset.MIGRATIONS)], "k", xlabel="Observed", ylabel="Model", linewidth=0.25)
-
     # N.Herts = "E07000099"
-    # Cambridge "E07000008"
-    lad = "E07000178" # Oxford
+    lad = "E07000178" 
+    lad_name = "Oxford"
     c = data.custom_snpp_variant[data.custom_snpp_variant.GEOGRAPHY_CODE == lad]
-    v.line((0,1), c.PROJECTED_YEAR_NAME, c.PEOPLE_SNPP, "k", label="baseline", xlabel="Year", ylabel="Population", title="Impact of scenario on population (%s)" % lad)
+    v.line((0,0), c.PROJECTED_YEAR_NAME, c.PEOPLE_SNPP, "k", label="baseline", xlabel="Year", ylabel="Population", title="Impact on {} ({})".format(lad_name, lad))
+    v.line((0,0), c.PROJECTED_YEAR_NAME, c.PEOPLE, "r", label="scenario")
+    v.panel((0,0)).legend() #("b","r"), ("base", "scenario"))
+    
+    lad = "E07000008" 
+    lad_name = "Cambridge"
+    c = data.custom_snpp_variant[data.custom_snpp_variant.GEOGRAPHY_CODE == lad]
+    v.line((0,1), c.PROJECTED_YEAR_NAME, c.PEOPLE_SNPP, "k", label="baseline", xlabel="Year", ylabel="Population", title="Impact on {} ({})".format(lad_name, lad))
     v.line((0,1), c.PROJECTED_YEAR_NAME, c.PEOPLE, "r", label="scenario")
     v.panel((0,1)).legend() #("b","r"), ("base", "scenario"))
 
@@ -63,14 +66,19 @@ def main(params):
     net_in = gdf[gdf.net_delta >= 0.0] 
     print("net in", len(net_in))
     v.polygons((0,2), net_in, xlim=[120000, 670000], ylim=[0, 550000], 
-      values=net_in.net_delta, clim=(0, np.max(net_in.net_delta)), cmap="Reds", edgecolor="darkgrey", linewidth=0.25)
+      values=net_in.net_delta, clim=(0, np.max(net_in.net_delta)), cmap="Reds", edgecolor="white", linewidth=0.1)
 
+    # model fit
+    v.scatter((1,0), model.dataset.MIGRATIONS, model.impl.yhat, "b.", markersize=3, 
+      title="%d %s migration model fit: R^2=%.2f" % (year, params["model_type"], model.impl.pseudoR2))
+    v.line((1,0), [0,max(model.dataset.MIGRATIONS)], [0,max(model.dataset.MIGRATIONS)], "k", xlabel="Observed", ylabel="Model", linewidth=0.25)
+
+    # actual OD migrations
     odmatrix = od_matrix(model.dataset, "MIGRATIONS", "O_GEOGRAPHY_CODE", "D_GEOGRAPHY_CODE")
-    v.matrix((1,0), np.log(odmatrix+1), cmap="Greens", xlabel="Destination", ylabel="Origin", title="Actual OD matrix (displaced log scale)")
+    v.matrix((1,1), np.log(odmatrix+1), cmap="Greens", xlabel="Destination", ylabel="Origin", title="Actual OD matrix (displaced log scale)")
+
+    # diff in OD migrations
     model_odmatrix = od_matrix(model.dataset, "MODEL_MIGRATIONS", "O_GEOGRAPHY_CODE", "D_GEOGRAPHY_CODE")
-    v.matrix((1,1), np.log(model_odmatrix+1), cmap="Greys", xlabel="Destination", ylabel="Origin", title="%s model OD matrix (displaced log scale)" % params["model_type"])
-    # construct new OD matrix
-    # TODO CHANGED_MIGRATIONS is zero in post-scenario years
     changed_odmatrix = od_matrix(model.dataset, "CHANGED_MIGRATIONS", "O_GEOGRAPHY_CODE", "D_GEOGRAPHY_CODE")
     delta_odmatrix = changed_odmatrix - model_odmatrix
     # we get away with log here as no values are -ve
