65670532cebe3550198d9ab113f227ab2af120b4
Tom Russell <tomalrussell@gmail.com>
2019-08-07 16:10:19 +0100

    Add scaling to postprocess script
    Following experimentation with simim input factors and scaling factors
    - see nismod/arc-scenarios notebooks.

--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 """Convert outputs to use as smif scenarios, extending with MYE for 2011-2015 and SNPP for NI
 
+Scales simim outputs to meet the baseline population-per-household on average across the Arc,
+which varies according to the dwelling scenarios. This ensures the total Arc population reaches
+the scenario target we want to explore, while using simim's outputs to give the proportional
+allocation of population migrating to and within the Arc.
+
 E.g. usage:
 
   $ cat config.json
@@ -33,44 +38,93 @@ def main(params):
     sys.exit()
 
   # get baseline 2011-2050, all UK
-  lads = pd.read_csv('./data/lad_nmcd_changes.csv')
+  lads = pd.read_csv("./data/lad_nmcd_changes.csv")
+  arc_lads = pd.read_csv("./data/scenarios/camkox_lads.csv")
   lad_cds = list(lads.lad16cd.unique())
   pop_mye = get_mye(lad_cds)
   pop_snpp = get_snpp(lad_cds)
   baseline = pd.concat([pop_mye, pop_snpp], axis=0)
-  baseline.population = baseline.population.astype(int)
-  baseline.to_csv(os.path.join(output_dir, 'arc_population__baseline.csv'), index=False)
+
+  baseline = prepare_for_output(baseline)
+  baseline.to_csv(os.path.join(output_dir, "arc_population__baseline.csv"), index=False)
 
   # add baseline pre-2016 and in Northern Ireland to simim scenarios
-  addition = baseline[(baseline.timestep < 2016) | baseline.lad_uk_2016.str.startswith('N')]
+  base_year = 2015
+  addition = baseline[
+    (baseline.timestep <= base_year) | baseline.lad_uk_2016.str.startswith("N")
+  ].copy()
+
+  baseline_dwl = pd.read_csv("./data/arc/arc_dwellings__baseline.csv")
+  baseline = baseline.merge(baseline_dwl, on=["timestep", "lad_uk_2016"])
 
   # read, rename, add baseline, output ready for smif
-  output_files = list(glob.glob(os.path.join(output_dir, 'simim_*.csv')))
+  output_files = list(glob.glob(os.path.join(output_dir, "simim_*.csv")))
   for output_file in output_files:
-    scenario = load_simim_output(output_file)
+    key = os.path.basename(
+      output_file
+    ).replace(
+      "simim_gravity_ppp_scenario", ""
+    ).replace(
+      "__gjh_D_HOUSEHOLDS-D_JOBS_ACCESSIBILITY-D_GVA_EX_LONDON__od_rail_b1__0.06.csv", ""
+    )
+    print(key)
+
+    scenario = load_simim_output(output_file, key)
+    scenario = scale(scenario, baseline, arc_lads)
+    scenario = scenario[scenario.timestep > base_year]
+    scenario = rename_columns(scenario)
     scenario = pd.concat(
       [scenario, addition], axis=0, sort=False
-    ).sort_values(
-      ['timestep', 'lad_uk_2016']
-    )[['timestep', 'lad_uk_2016', 'population']]
-    scenario.population = scenario.population.astype(int)
+    )
 
-    # print(scenario.timestep.unique())
-    # print(len(scenario), len(scenario.lad_uk_2016.unique()), len(scenario.timestep.unique()))
-
-    key = os.path.basename(output_file).replace('simim_gravity_ppp_scenario', '').replace('.csv', '')
-    print(key)
-    name_lu = {
-      '0e': '0-unplanned',
-      '1e': '1-new-cities',
-      '2e': '2-expansion'
-    }
-    if key in name_lu:
-      fname = "arc_population__{}.csv".format(name_lu[key])
-    else:
-      fname = "arc_population__{}.csv".format(key)
-
+    scenario = prepare_for_output(scenario)
+    fname = "arc_population__{}.csv".format(key)
     scenario.to_csv(os.path.join(output_dir, fname), index=False)
+
+
+def scale(scenario, baseline, arc_lads):
+  # stitch scenario and baseline together
+  scenario = scenario.copy()
+  scenario["scenario"] = "scenario"
+  baseline = baseline.copy()
+  baseline["scenario"] = "baseline"
+
+  full = pd.concat([scenario, baseline], axis=0, sort=True)
+  # filter to Arc only
+  dataset = full[full.lad_uk_2016.isin(arc_lads.geo_code)].copy()
+  # calculate people per household
+  dataset["pph"] = dataset.population / dataset.dwellings
+  # group to summarise
+  summary = dataset.groupby(["timestep","scenario"]).sum()
+  summary["pph"] = summary.population / summary.dwellings
+  # merge on baseline
+  summary = summary.reset_index()
+  summary = summary.merge(
+          summary[summary.scenario == "baseline"][["timestep","pph"]],
+          on="timestep", how="left", suffixes=("","_baseline"))
+  # calculate expected population from scenario households and baseline people-per-household
+  summary["expected_population"] = summary.dwellings * summary.pph_baseline
+  # calculate scale factor
+  summary["people_scale_factor"] = summary.expected_population / summary.population
+  # merge summary back to main dataset
+  dataset = dataset.merge(
+      summary[["timestep","scenario", "people_scale_factor"]],
+      on=["timestep","scenario"], how="left")
+  # calculate scaled population
+  dataset["scaled_population"] = dataset.population * dataset.people_scale_factor
+  # rename, filter, concatenate back into rest of scenario output
+  dataset = dataset[dataset.scenario != "baseline"]
+  dataset = dataset[["timestep","lad_uk_2016","scaled_population"]] \
+    .rename(columns={"scaled_population": "population"})
+
+  other_scenario = scenario[~scenario.lad_uk_2016.isin(arc_lads.geo_code)] \
+    [["timestep", "lad_uk_2016", "population"]]
+
+  dataset = pd.concat([
+    dataset,
+    other_scenario
+  ], axis=0, sort=True)
+  return dataset
 
 
 def get_mye(lad_cds):
@@ -94,17 +148,29 @@ def get_snpp(lad_cds):
   return pd.concat([pop_ex, pop_snpp], axis=0)
 
 
-def load_simim_output(filepath):
-  df = rename_columns(pd.read_csv(filepath)).drop('PEOPLE_SNPP', axis=1)
+def load_simim_output(filepath, scenario_key):
+  df_pop = rename_columns(pd.read_csv(filepath).drop("PEOPLE_SNPP", axis=1))
+  df_dwl = pd.read_csv("./data/arc/arc_dwellings__{}.csv".format(scenario_key))
+  df = df_pop.merge(df_dwl, on=["timestep", "lad_uk_2016"])
+
   return df
+
+
+def prepare_for_output(df):
+  df.population = df.population.astype(int)
+  return df.sort_values(
+      ["timestep", "lad_uk_2016"]
+    )[
+      ["timestep", "lad_uk_2016", "population"]
+    ]
 
 
 def rename_columns(df):
   return df.rename(columns={
-    'GEOGRAPHY_CODE': 'lad_uk_2016',
-    'PROJECTED_YEAR_NAME': 'timestep',
-    'PEOPLE': 'population',
-    'OBS_VALUE': 'population'
+    "GEOGRAPHY_CODE": "lad_uk_2016",
+    "PROJECTED_YEAR_NAME": "timestep",
+    "PEOPLE": "population",
+    "OBS_VALUE": "population"
   })
 
 
